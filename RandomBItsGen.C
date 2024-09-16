dd19f0d6d87682b7f3f1c85c11da395015b00250
Marleen Siebonscd <m.s.j.siebons@student.utwente.nlcd>
2024-09-16 12:36:24 +0200

    Floating point exeption have to figure out where..

--- RandomBItsGen.C
+++ RandomBItsGen.C
@@ -1,18 +1,43 @@
+// load useful libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+// initiate functions
+int getRandomDec();
 
+int convertDecBin();
+// Declare functions used in the main part of the code
+int getRandomDec(int nmax) {
+    srand(time(NULL));
+    int range= pow(2,nmax);
+    int r1 = rand()% range; //will be a random number from 0 to 1
+    printf("r1 is now: %d", r1);
+    return r1;
 
-int main() {
-    srand(time(NULL)); 
-    double r1= rand(); //will be a random number from 0 to 1
-    printf("our random: %lf", r1);
-    printf("we have r1 divided by randmax: %lf", r1/RAND_MAX);
-    if (r1/RAND_MAX > 0.5) {
-        printf("We should return 1");
-        }
-    else {
-       printf("we should return 0");
-    }
-    // printf("test rand %lf", r1/RAND_MAX);
 }
+
+int convertDecBin(int decimal, int nBitsLim){
+    int answer=0;
+    while (nBitsLim >=0){
+        int power2=pow(2, nBitsLim-1);
+        int dec= decimal%power2;
+        int power10= pow(10, nBitsLim-1);
+        int addterm= dec*power10;
+        answer= answer + addterm ;
+        if (dec!=0){
+            decimal=decimal-dec;
+        }
+        nBitsLim=nBitsLim-1;
+    }
+    return answer;
+}
+
+// Start the main part of the code
+int main() {
+    int nBits= 8;
+    int decimalnum= getRandomDec(nBits);
+    printf("We defined a function, lets test: %d", getRandomDec(nBits));
+    printf("Made a converter, lets test it: %d", convertDecBin(decimalnum, nBits));
+}
+
